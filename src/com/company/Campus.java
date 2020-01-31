9c98b92625625945c153e3ec3ce830bf74f12419
qatheboss <quanganh.nguyen@saintandrews.net>
2020-01-30 21:00:54 -0500

    asdf

--- src/com/company/Campus.java
+++ src/com/company/Campus.java
@@ -3,16 +3,48 @@ import java.awt.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.PriorityQueue;
 
 public class Campus {
+
+     private class Route implements Comparable {
+         public ArrayList<String> soFar;
+         public int distance;
+
+         public Route()  {
+             soFar = new ArrayList<String>();
+             distance = 0;
+         }
+
+         public Route(Route o) {
+             soFar = new ArrayList<String>();
+             distance = o.distance;
+             for (String s : o.soFar) {
+                 soFar.add(s);
+             }
+         }
+
+
+         @Override
+         public int compareTo(Object o) {
+             Route other = (Route) o;
+             return distance - ((Route) o).distance;
+         }
+
+         public String getCurrent() {
+             return soFar.get(soFar.size()-1);
+         }
+     }
 
 
     private class Location {
         public ArrayList<Path> paths;
         public String label;
+        public boolean visted;
         public Location (String label) {
             this.label = label;
             paths = new ArrayList<Path>() ;
+            visted = false;
         }
         @Override
         public String toString() {
@@ -66,4 +98,33 @@ public class Campus {
         return false;
     }
 
+    public ArrayList<String> doDijkstra (String start, String end) {
+        for (HashMap.Entry<String, Location> locs : locations.entrySet()) {
+            locs.getValue().visted = false;
+        }
+        PriorityQueue<Route> toDo = new PriorityQueue<Route>();
+        Route first = new Route();
+        first.soFar.add(start);
+        toDo.add(first);
+        while (toDo.isEmpty() == false) {
+            Route nextRoute = toDo.remove();
+            String atLoc = nextRoute.getCurrent();
+            if (atLoc.equals(end)) {
+                System.out.println("Path found, distance: " + nextRoute.distance);
+                return nextRoute.soFar;
+            }
+            ArrayList<Path> paths = locations.get(atLoc).paths;
+            for(Path p : paths) {
+                if (p.target.visted == true) {
+                    continue;
+                }
+                p.target.visted = true;
+                Route newRoute = new Route(nextRoute);
+                newRoute.soFar.add(p.target.label);
+                newRoute.distance += p.distance;
+                toDo.add(newRoute);
+            }
+        }
+        return null;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,8 @@
 package com.company;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
+    public static Campus makeCampus() {
         Campus c = new Campus();
         c.addPath("Chapel", "Alumni", 2);
         c.addPath("Chapel", "Boys", 1);
@@ -38,7 +37,53 @@ public class Main {
 
         c.addPath("Sci", "Boys", 1);
 
+        return c;
+    }
 
+    public static Campus makeRandomCampus() {
+        Campus c = new Campus();
+        c.addPath("Chapel", "Alumni", 1 + (int)(5*Math.random()));
+        c.addPath("Chapel", "Boys", 1 + (int)(5*Math.random()));
+        c.addPath("Chapel", "Cohen", 1 + (int)(5*Math.random()));
+
+        c.addPath("Alumni", "Admin", 1 + (int)(5*Math.random()));
+        c.addPath("Alumni", "Girls dorm", 1 + (int)(5*Math.random()));
+
+        c.addPath("Girls dorm", "VA", 1 + (int)(5*Math.random()));
+        c.addPath("Girls dorm", "Teacher's lot", 1 + (int)(5*Math.random()));
+
+        c.addPath("VA", "US classes", 1 + (int)(5*Math.random()));
+
+        c.addPath("US classes", "Teacher's lot", 1 + (int)(5*Math.random()));
+        c.addPath("US classes", "Admin", 1 + (int)(5*Math.random()));
+
+        c.addPath("Teacher's lot", "Security Gate", 1 + (int)(5*Math.random()));
+
+        c.addPath("Admin", "Senior lot", 1 + (int)(5*Math.random()));
+        c.addPath("Admin", "Security Gate", 1 + (int)(5*Math.random()));
+
+        c.addPath("Senior lot", "Security Gate", 1 + (int)(5*Math.random()));
+
+        c.addPath("Security Gate", "Rock", 1 + (int)(5*Math.random()));
+
+        c.addPath("Rock", "Cohen", 1 + (int)(5*Math.random()));
+        c.addPath("Rock", "Dining Hall", 1 + (int)(5*Math.random()));
+
+        c.addPath("Cohen", "Dining Hall", 1 + (int)(5*Math.random()));
+
+        c.addPath("Dining Hall", "Sci", 1 + (int)(5*Math.random()));
+
+        c.addPath("Sci", "Boys", 1 + (int)(5*Math.random()));
+
+        return c;
+
+
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        Campus c = makeCampus();
+        Campus cRand = makeRandomCampus();
         System.out.println(c.getPaths("Chapel"));
         System.out.println(c.getPaths("Alumni"));
         System.out.println(c.getPaths("Girls dorm"));
@@ -53,7 +98,10 @@ public class Main {
         System.out.println(c.getPaths("Cohen"));
         System.out.println(c.getPaths("Sci"));
         System.out.println(c.getPaths("Boys"));
-
+        ArrayList<String> normalPath = c.doDijkstra("Girls dorm", "Rock");
+        System.out.println("Regular: " + normalPath);
+        ArrayList<String> randPath = cRand.doDijkstra("Girls dorm", "Rock");
+        System.out.println("Random: " + randPath);
         String current = "Security Gate";
         Scanner sc = new Scanner(System.in);
         while (true) {
